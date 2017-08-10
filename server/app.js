a8d6afb068699c983962612d92d3c7ae7231e812
TAksenov <taksenov@gmail.com>
2017-08-10 17:40:23 +0500

    add babel syntax and eslinter support

--- server/app.js
+++ server/app.js
@@ -2,3 +2,7 @@ import express from 'express';
 
 const app = express();
 
+const SERVER_PORT = 8080;
+const server = app.listen(SERVER_PORT, () => {
+	console.log(`Server is up and running on ${SERVER_PORT}`);
+});
