ed9c69bc6a2c078a2ae2d58a2e6980ee343fdd4f
TAksenov <taksenov@gmail.com>
2017-08-10 17:57:26 +0500

    add spaces for inits

--- server/app.js
+++ server/app.js
@@ -4,6 +4,6 @@ const app = express();
 
 const SERVER_PORT = 8080;
 const server = app.listen(SERVER_PORT, () => {
-	console.log(`Server is up and running on ${SERVER_PORT}`);
+    console.log(`Server is up and running on ${SERVER_PORT}`);
 });
 
