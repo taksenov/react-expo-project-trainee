2aadd34978cf74441c814578b2803b4a9dc720df
TAksenov <taksenov@gmail.com>
2017-08-11 15:56:09 +0500

    separate settings to config

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import {serverPort} from '../etc/config.json';
+
 import * as db from './utils/DataBaseUtils.js';
 
 db.setUpConnection();
 
 const app = express();
+const SERVER_PORT = +serverPort;
 
 app.use( bodyParser.json() );
 
@@ -21,7 +24,6 @@ app.delete('/notes/:id', (req, res) => {
     db.deleteNote(req.params.id).then( data => res.send(data) );
 });
 
-const SERVER_PORT = 8080;
 const server = app.listen(SERVER_PORT, () => {
     console.log(`Server is up and running on ${SERVER_PORT} port`);
 });
--- server/utils/DataBaseUtils.js
+++ server/utils/DataBaseUtils.js
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 
+import config from '../../etc/config.json';
+
 import '../models/Note';
 
 const Note = mongoose.model('Note');
 
 export function setUpConnection() {
     mongoose.Promise = global.Promise; // workAround from https://github.com/Automattic/mongoose/issues/4291
-    mongoose.connect('mongodb://localhost/notes', {
+    mongoose.connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`, {
         useMongoClient: true,
     });
 } //setUpConnection
