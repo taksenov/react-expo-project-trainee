7726ac1fba2ff3b6b996d37e859f7892c7d872ad
TAksenov <taksenov@gmail.com>
2017-08-15 13:38:42 +0500

    add bootstrap@3 and bootswatchTheme

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import '../commonless/bootswatchTheme/main.less';
+//        /commonless/bootswatchTheme/main.less
+
 ReactDOM.render(
     <h1>Notes</h1>,
     document.getElementById('mount-point')
