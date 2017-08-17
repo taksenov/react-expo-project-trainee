6b6b93c4ff43f7d2ff989ec2b7536582beea109d
TAksenov <taksenov@gmail.com>
2017-08-17 14:57:30 +0500

    NoteEditor setup for React ES6 classes

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -4,16 +4,35 @@ import NoteEditor from '../NoteEditor/NoteEditor.jsx';
 import NotesGrid from '../NotesGrid/NotesGrid.jsx';
 
 class App extends React.Component {
+    handleNoteAdd(data) {
+        console.log(data);
+    }
+    
     render() {
         return (
             <div className='App'>
+            
+                {/* Header */}
                 <div className='row'>
                     <div className='col-lg-12 text-center'>
                         <h2 className='App__header'> Notes </h2>
                     </div>
                 </div>
-                <NoteEditor />
-                <NotesGrid />
+                
+                {/* NoteEditor */}
+                <div className='row'>
+                    <div className='col-lg-12 text-center'>
+                        <NoteEditor onNoteAdd={this.handleNoteAdd} />
+                    </div>
+                </div>
+            
+                {/* notesGrid */}
+                <div className='row'>
+                    <div className='col-lg-12 text-center'>
+                        <NotesGrid />
+                    </div>
+                </div>
+            
             </div>
         );
     }
--- client/components/NoteEditor/NoteEditor.jsx
+++ client/components/NoteEditor/NoteEditor.jsx
@@ -1,11 +1,69 @@
 import React from 'react';
 
+import './NoteEditor.style.less';
+
 class NoteEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            text: '',
+            color: '#FFFFFF'
+        };
+    }
+    
+    handleTextChange(event) {
+        this.setState({ text: event.target.value });
+    }
+    
+    handleTitleChange(event) {
+        this.setState({ title: event.target.value });
+    }
+    
+    handleNoteAdd() {
+        const newNote = {
+            title: this.state.title,
+            text: this.state.text,
+            color: this.state.color            
+        };
+        
+        this.props.onNoteAdd(newNote);
+        this.setState({
+            title: '',
+            text: '',
+            color: '#FFFFFF'
+        });
+    }
+    
     render() {
         return (
-            <h1> NoteEditor </h1>
+            <div className='NoteEditor'>
+                <input 
+                    type='text'
+                    className='NoteEditor__title'
+                    placeholder='Enter title'
+                    value={this.state.title}
+                    onChange={(e) => this.handleTitleChange(e)}
+                />
+                <textarea 
+                    placeholder='Enter note text'
+                    rows={5} 
+                    className='NoteEditor__text'
+                    value={this.state.text}
+                    onChange={(e) => this.handleTextChange(e)}
+                />
+                <div className='NoteEditor__footer'>
+                    <button 
+                        className='NoteEditor__button'
+                        disabled={!this.state.text}
+                        onClick={() => this.handleNoteAdd()}
+                    >
+                        Add
+                    </button>
+                </div>
+            </div>
         );
     }
-} //App
+} //NoteEditor
 
 export default NoteEditor;
