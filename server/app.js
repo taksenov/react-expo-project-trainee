5c65f599708d3b25dee041b6499f97b11ab00175
TAksenov <taksenov@gmail.com>
2017-08-17 17:39:15 +0500

    add cors

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import {serverPort} from '../etc/config.json';
 
@@ -11,6 +12,7 @@ const app = express();
 const SERVER_PORT = +serverPort;
 
 app.use( bodyParser.json() );
+app.use( cors({ origin: '*' }) );
 
 app.get('/notes', (req, res) => {
     db.listNotes().then( data => res.send(data) );
