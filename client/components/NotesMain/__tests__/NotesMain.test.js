60b8e215fc7fe00369d3a801d7f37d47c2b5a708
TAksenov <taksenov@gmail.com>
2017-08-31 16:57:19 +0500

    add mount from enzyme

--- client/components/NotesMain/__tests__/NotesMain.test.js
+++ client/components/NotesMain/__tests__/NotesMain.test.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import NotesMain from '../NotesMain.jsx';
+import NoteEditor from '../../NoteEditor/NoteEditor.jsx';
+import NotesGrid from '../../NotesGrid/NotesGrid.jsx';
 
+// Тестирование на рендеринг компонента
 describe('<NotesMain />', () => {
     it('should render NotesMain', () => {
         const renderedComponent = shallow(
@@ -14,5 +17,36 @@ describe('<NotesMain />', () => {
         // Find NotesMain Page
         expect(renderedComponent.find('div .EddsData__NotesMain').hasClass('EddsData__NotesMain')).toBe(true);
 
+    });
+});
+
+
+// import Home from './Home';
+// import Wellcome from './Wellcome';
+
+describe('<NotesMain />', () => {
+    it('should fetch childComponents on mount', () => {
+        // const fetchUsernameSpy = jest.fn(cb => 'Aliсe');
+        // const renderedComponent = mount(
+        //     <Home
+        //         username={'Aliсe'}
+        //         changeUsername={jest.fn()}
+        //     />
+        // );
+        
+        // // Выведем отрендеренный комонент
+        // console.log(renderedComponent.debug());
+        
+        // expect(fetchUsernameSpy).toBeCalled();\
+        const renderedComponent = mount(
+            <NotesMain />
+        );
+
+        // // Выведем отрендеренный компонент
+        // console.log(renderedComponent.debug());
+
+        // Find NotesMain Page
+        expect(renderedComponent.find('div .EddsData__NotesMain').hasClass('EddsData__NotesMain')).toBe(true);
+        
     });
 });
